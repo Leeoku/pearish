6a4805e12dba7d48462e8207fae13c55d88e5dc4
Kenneth Lee <leeoku@gmail.com>
2020-06-16 16:04:39 -0400

    Fix code to address PR

--- parse.py
+++ parse.py
@@ -1,9 +1,10 @@
-import requests, itertools
-import json
+import requests, json
 from api import ocr_key
 
 
-def ocr_space_file(filename, overlay=False, api_key= ocr_key, language='eng', istable=True, scale = True):
+def ocr_space_file(
+    filename, overlay=False, api_key=ocr_key, language="eng", istable=True, scale=True
+):
     """ OCR.space API request with local file.
         Python3.5 - not tested on 2.7
 
@@ -19,20 +20,22 @@ def ocr_space_file(filename, overlay=False, api_key= ocr_key, language='eng', is
     :param istable: parses output as a table
     """
 
-    payload = {'isOverlayRequired': overlay,
-               'apikey': api_key,
-               'language': language,
-               'istable': istable,
-               }
-    with open(filename, 'rb') as f:
-        r = requests.post('https://api.ocr.space/parse/image',
-                          files={filename: f},
-                          data=payload,
-                          )
+    payload = {
+        "isOverlayRequired": overlay,
+        "apikey": api_key,
+        "language": language,
+        "istable": istable,
+    }
+    with open(filename, "rb") as f:
+        r = requests.post(
+            "https://api.ocr.space/parse/image", files={filename: f}, data=payload,
+        )
     return r.content.decode()
 
 
-def ocr_space_url(url, overlay=False, api_key= ocr_key, language='eng', isTable=True):
+def ocr_space_url(
+    url, overlay=False, api_key=ocr_key, language="eng", isTable=True, scale=True
+):
     """ OCR.space API request with remote file.
         Python3.5 - not tested on 2.7
 
@@ -48,27 +51,25 @@ def ocr_space_url(url, overlay=False, api_key= ocr_key, language='eng', isTable=
     :param isTable: parses output as a table
     """
 
-    payload = {'url': url,
-               'isOverlayRequired': overlay,
-               'apikey': api_key,
-               'language': language,
-               'isTable': isTable,
-               }
-    r = requests.post('https://api.ocr.space/parse/image',
-                      data=payload,
-                      )
-    #return r.content.decode()
+    payload = {
+        "url": url,
+        "isOverlayRequired": overlay,
+        "apikey": api_key,
+        "language": language,
+        "isTable": isTable,
+    }
+    r = requests.post("https://api.ocr.space/parse/image", data=payload,)
+    # return r.content.decode()
     return r.json()
 
-data = ocr_space_url('https://ocr.space/Content/Images/receipt-ocr-original.jpg')
-
-#print(data.get('ParsedResults')[0]['TextOverlay']['Lines'][22].get('LineText'))
-words = []
-
-for i in data.get('ParsedResults')[0]['TextOverlay']['Lines']:
-    words.append(i.get('LineText'))
-
-#print(i.get('LineText'))
+def parse():
+    words = []
+    data = ocr_space_url("https://ocr.space/Content/Images/receipt-ocr-original.jpg")
+    for i in data.get("ParsedResults")[0]["TextOverlay"]["Lines"]:
+        words.append(i.get("LineText"))
+        
+# print(data.get('ParsedResults')[0]['TextOverlay']['Lines'][22].get('LineText'))
+# print(i.get('LineText'))
 # print((new_data["ParsedResults"][0].values()))
 # filtered_data = (new_data["ParsedResults"][0].get('LineText'))
 # data['Words'] = [json.loads(s) for s in data['Words']]
@@ -78,3 +79,6 @@ for i in data.get('ParsedResults')[0]['TextOverlay']['Lines']:
 #     raw_data = json.load(json_file)
 # with open('rawdata.json', 'w') as f:
 #     f.write(json.dump(data,f))
+
+if __name__ == '__main__':
+    parse()
