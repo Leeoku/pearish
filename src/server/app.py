a2aadd72c95c180010ad562436955b5112d31b98
Omar Moghazy <omar.moghazy.42@gmail.com>
2020-06-20 21:09:24 +0200

    tested and made sure inserting in db works

--- src/server/app.py
+++ src/server/app.py
@@ -3,11 +3,25 @@ import pymongo
 from flask import Flask
 from pprint import pprint
 
-ADMIN_NAME = os.environ["ADMIN_NAME"]
-PASSWORD = os.environ["PASSWORD"]
+ADMIN_NAME = "SGP"
+PASSWORD = "UxmwrTNrtbmrHzGp"
 
 app = Flask(__name__)
 uri = "mongodb+srv://{}:{}@stackedup-nr3iv.mongodb.net/StackedUp?retryWrites=true&w=majority".format(ADMIN_NAME, PASSWORD)
+
+# connect to db and get cluster
 cluster = pymongo.MongoClient(uri)
+
+# get db from cluster 
 db = cluster['StackedUp']
+
+# get collection from db
 collection = db['users']
+
+# testing insert
+
+# collection.insert_one({"_id": 0, "user-name":"omar", "user_items": [
+#             {"name":"bread", "category": "carb",
+#             "purchase_date": "9/6/2020", "expiration_date":"22/6/2020"}
+#       ]
+# })
