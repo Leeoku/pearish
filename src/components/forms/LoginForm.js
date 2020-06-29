f7d9f48bbbaa24c7f20b5faf9a408d289ca5484d
Maaz Q <maazq210@gmail.com>
2020-06-29 16:40:44 -0400

    added prop-types to package.json and error messages for login

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {Form, Button, Input} from 'semantic-ui-react';
+import Validator from 'validator';
+import InlineError from '../messages/InlineError'
+
 class LoginForm extends React.Component {
 
   state = {
@@ -14,12 +17,24 @@ class LoginForm extends React.Component {
   onChange= event => this.setState({data: {...this.state.data, [event.target.name]: event.target.value
   }});
 
+
+  onSubmit = () => {
+    const errors = this.validate(this.state.data);
+    this.setState({errors});
+  };
+
+  validate = (data) => {
+    const errors = {};
+    if (!Validator.isEmail(data.email)) errors.email = "Invalid email";
+    if (!data.password) errors.password = 'Password required';
+    return errors;
+  }
   render() {
-    const {data} = this.state;
+    const {data, errors} = this.state;
 
     return (
-      <Form>
-        <Form.Field>
+      <Form onSubmit={this.onSubmit}>
+        <Form.Field error={!!errors.email}>
           <label htmlFor="email">Email</label>
           <input
             type="email"
@@ -29,8 +44,9 @@ class LoginForm extends React.Component {
             value={data.email}
             onChange={this.onChange}
             />
+            {errors.email && <InlineError text={errors.email} />}
         </Form.Field>
-        <Form.Field>
+        <Form.Field error={!!errors.password}>
           <label htmlFor="password">Password</label>
           <input
             type="password"
@@ -40,6 +56,7 @@ class LoginForm extends React.Component {
             value={data.password}
             onChange={this.onChange}
             />
+            {errors.password && <InlineError text={errors.password} />}
         </Form.Field>
         <Button primary>Login</Button>
       </Form>
