0f8ece8ba5e8b86f95a642a8e0baa802d5f4d49e
Omar Moghazy <omar.moghazy.42@gmail.com>
2020-06-30 18:57:48 +0200

    ejected react app, deployed it on the flask server, still cannot parse the response from flask

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br />
+    <p>name: {window.user}</p>
         </p>
         <a
           className="App-link"
--- src/server/app.py
+++ src/server/app.py
@@ -3,6 +3,7 @@ import pymongo
 import flask
 from pprint import pprint
 import api
+import json
 
 app = flask.Flask(__name__)
 uri = "mongodb+srv://{}:{}@stackedup-nr3iv.mongodb.net/StackedUp?retryWrites=true&w=majority".format(api.ADMIN_NAME, api.PASSWORD)
@@ -18,16 +19,15 @@ collection = db['users']
 
 # testing insert
 
-# collection.insert_one({"_id": 0, "user-name":"omar", "user_items": [
+# collection.insert_one({"_id": 0, "user_name":"omar", "user_items": [
 #             {"name":"milk", "category": "dairy",
 #             "purchase_date": "10/6/2020", "expiration_date":"23/6/2020"}
 #       ]
 # })
 
-# collection.find_one('{_id: 1}')
-
+test = collection.find_one({"_id":1})
 @app.route("/")
 def my_index():
-    return flask.render_template("index.html", token="hello flask+react")
+    return flask.render_template("index.html", token=flask.json.loads(flask.json.dumps(test)))
 
 app.run(debug=True)
