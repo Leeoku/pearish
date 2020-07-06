61b4a57e549d253d7f1e251a132000158e2ef910
hackerrank <me@hackerrank.com>
2020-07-06 16:25:56 -0700

    NavBar links work. Still need to change position of headers on pages

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import {  Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
 import AboutPage from './components/pages/AboutPage';
-
+import'semantic-ui-css/semantic.min.css';
 const App = () => (
-    <div>
+    <div class = "ui secondary menu">
+      <a class = "active item"
+         href = "/" >Home</a>
+      <a class ="item"
+         href = "/login">Login</a>
+      <a class = "item"
+         href = "/about">About</a>
         <Route path="/" exact component={HomePage} />
         <Route path="/login" exact component={LoginPage} />
         <Route path="/about" exact component={AboutPage} /> 
