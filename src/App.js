9a7ea062a900d71ac76d73eab4f27302efd4d438
hackerrank <me@hackerrank.com>
2020-07-06 15:49:33 -0700

    deleted index.html

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import {  Route } from 'react-router-dom';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
 import AboutPage from './components/pages/AboutPage';
 
 const App = () => (
-  <div className="ui container">
-    <Route path="/" exact component={HomePage} />
-    <Route path="/login" exact component={LoginPage} />
-    <Route path="/about" exact component={AboutPage} />
-  </div>
+    <div>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/login" exact component={LoginPage} />
+        <Route path="/about" exact component={AboutPage} /> 
+    </div>
 );
 
 export default App;
