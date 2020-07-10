6f93ec2060d23f66a04a08312eb1ae8237ef731f
hackerrank <me@hackerrank.com>
2020-07-10 13:27:56 -0700

    links deleted off homepage.js

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,52 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
 
 const HomePage = () => (
   <div>
     <h1 className ="page-title"> Pearish </h1>
-    <Link to="/login">Login</Link>
-    <Link to="/about">About</Link>
   </div>
 );
-/*
-class App extends Component {
-  render () {
-    return (
-      <div class="container">
-	       <div class="row">
-	        <div class="col-md-6">
-	         <form method="post" action="#" id="#">
-              <div class="form-group files">
-                <label>Upload Your File </label>
-                <input type="file" class="form-control" multiple="" />
-              </div>
-
-
-          </form>
-
-
-	         </div>
-	          <div class="col-md-6">
-	           <form method="post" action="#" id="#">
-
-
-
-
-              <div class="form-group files color">
-                <label>Upload Your File </label>
-                <input type="file" class="form-control" multiple="" />
-              </div>
-
-
-          </form>
-
-
-	         </div>
-	          </div>
-            </div>
-    );
-  }
-}
-*/
 export default HomePage;
