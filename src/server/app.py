72b96fd080cf4306861d31e706a07bf2929c7ca9
Kenneth Lee <leeoku@gmail.com>
2020-07-09 20:41:20 -0400

    kens attempt

--- src/server/app.py
+++ src/server/app.py
@@ -3,13 +3,16 @@ import pymongo
 import flask
 from pprint import pprint
 import api
-import json
+import json, collections
+import bson
+#import bsonjs
+#from bson.json_util import dumps as bdumps
 
 app = flask.Flask(__name__)
-uri = "mongodb+srv://{}:{}@stackedup-nr3iv.mongodb.net/StackedUp?retryWrites=true&w=majority".format(api.ADMIN_NAME, api.PASSWORD)
+url = "mongodb+srv://{}:{}@stackedup-nr3iv.mongodb.net/StackedUp?retryWrites=true&w=majority".format(api.ADMIN_NAME, api.PASSWORD)
 
 # connect to db and get cluster
-cluster = pymongo.MongoClient(uri)
+cluster = pymongo.MongoClient(url)
 
 # get db from cluster 
 db = cluster['StackedUp']
@@ -25,9 +28,24 @@ collection = db['users']
 #       ]
 # })
 
-test = collection.find_one({"_id":1})
+test = collection.find_one({"_id":0})
+print(test)
+d = bson.encode(test)
+print(d)
+d_text = bson.decode(d)
+print(d_text)
+# d = bdumps(test)
+# #raw_bson = bsonjs.loads(test)
+# print(test)
+# print('Test is of type ' + str(type(test)))
+# print('Json.dumps(test) is of type ' + str(type(json.dumps(test))))
+# print(chr(d))
+# print(type(d))
+
+
+
 @app.route("/")
 def my_index():
-    return flask.render_template("index.html", token=flask.json.loads(flask.json.dumps(test)))
-
+    #return flask.render_template("index.html", token=flask.json.loads(flask.json.dumps(test)))
+    return flask.render_template("index.html", token= d_text)
 app.run(debug=True)
