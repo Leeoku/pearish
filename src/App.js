e548b05c5a6d46b8fdcf5f1aa81eb48c3c29c707
Kenneth Lee <leeoku@gmail.com>
2020-07-10 15:11:34 -0400

    fix jinja template to public index.html

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br />
-    <p>name: {window.user}</p>
+    <p> TEST name: {window.user}</p>
         </p>
         <a
           className="App-link"
