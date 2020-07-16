cdca32678880266be7b5c91750ab94a2de0d2b1d
Kenneth Lee <leeoku@gmail.com>
2020-07-16 18:48:18 -0400

    modify app

--- src/server/app.py
+++ src/server/app.py
@@ -6,7 +6,9 @@ import api
 import json, collections
 import bson
 #import bsonjs
-#from bson.json_util import dumps as bdumps
+from bson import json_util
+from flask_restful import Api, Resource
+from flask.json import JSONEncoder, jsonify
 
 app = flask.Flask(__name__)
 url = "mongodb+srv://{}:{}@stackedup-nr3iv.mongodb.net/StackedUp?retryWrites=true&w=majority".format(api.ADMIN_NAME, api.PASSWORD)
@@ -33,4 +35,55 @@ test = collection.find_one({"_id":0})
 @app.route("/")
 def my_index():
     return flask.render_template("index.html", token= test)
-app.run(debug=True)
+
+#Response to get all user items, mapped to /user/
+class UserCollection(Resource):
+    def get(self):
+        #user = collection.find_one({"user_name": user_name})
+        container = []
+        for user in collection.find():
+            container.append(user)
+        return json.loads(json_util.dumps(container))
+
+#Response to add an entry, mapped to /user/create/<user_name>
+class UserCollectionCreate(Resource):
+#NEED TO ADD PAYLOAD FROM PARSE.PY AND IDENTIFY WHICH USER IT IS, THIS POST IS A PLACEHOLDER
+    def post(self, user_name):
+        payload = {
+            "user_name":user_name,
+            "user_items": [{
+                "name": "",
+                "cateogry": "",
+                "purchase_date": "",
+                "expiration_date": ""
+            }]
+        }
+        collection.insert(payload)
+        return f"{user_name} added"
+
+#Response to get, update and delete one user
+class UserCollectionName(Resource):
+    def get(self, user_name):
+        user = collection.find_one({"user_name": user_name})
+        #return {"User": user}
+        return json.loads(json_util.dumps(user))
+    def delete(self, user_name):
+        collection.delete_one({"user_name": user_name})
+        return f"{user_name} deleted"
+    def post(self, user_name):
+        collection.update_one({"user_name": user_name})
+        return f"{user_name} updated"
+
+#Response to get and delete items
+class UserCollectionItems(Resource):
+    def get(self, user_name):
+        user = collection.find_one({"user_name": user_name})
+        items = user["user_items"]
+        return{"Items": items}
+
+restful_api.add_resource(UserCollection, '/user/')
+restful_api.add_resource(UserCollectionCreate, '/user/create/<string:user_name>')
+restful_api.add_resource(UserCollectionName, '/user/<string:user_name>')
+restful_api.add_resource(UserCollectionItems, '/user/<string:user_name>/items')
+if __name__ == '__main__':
+    app.run(debug = True)
