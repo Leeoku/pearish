ee038df9130d398345218f78f2b9ad1cf12ffd18
hackerrank <tylerhoh@gmail.com>
2020-07-20 17:36:14 -0700

    resolved merge conflicts on landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
 import UploadForm from "./UploadForm";
-=======
->>>>>>> Initial Commit for Flask+MongoDB
 
 class Landing extends Component {
   render() {
@@ -10,14 +7,9 @@ class Landing extends Component {
       <div className="container">
         <div className="jumbotron mt-5">
           <div className="col-sm-8 mx-auto">
-<<<<<<< HEAD
             <h1 className="text-center">WELCOME TO YOUR DIGITAL PANTRY</h1>
           </div>
           <UploadForm></UploadForm>
-=======
-            <h1 className="text-center">WELCOME</h1>
-          </div>
->>>>>>> Initial Commit for Flask+MongoDB
         </div>
       </div>
     );
