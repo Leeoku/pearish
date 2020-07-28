4ddf47dd4c59755e0a23e89e78e1986cffb98bb2
Maaz Q <maazq210@gmail.com>
2020-07-27 20:56:09 -0400

    show items in state

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -16,21 +16,24 @@ class Profile extends Component {
   componentDidMount() {
     const token = window.localStorage.getItem("usertoken");
     const decoded = jwt_decode(token);
-    this.getItem();
     this.setState({
       first_name: decoded.identity.first_name,
       last_name: decoded.identity.last_name,
       email: decoded.identity.email,
     });
+    this.getItem({
+      email: decoded.identity.email,
+    });
   }
 
-  getItem() {
+  getItem(email) {
     axios
-      .get("http://localhost:5000/users/<string:user_name>")
+      .get("http://localhost:5000/user/" + encodeURIComponent(email.email))
       .then((response) => {
         const user_items = response.data;
         console.log(user_items);
         JSON.stringify(user_items);
+        console.log(JSON.stringify(user_items));
         this.setState(user_items);
         console.log("Data received");
       })
@@ -63,9 +66,9 @@ class Profile extends Component {
               <tr>
                 <td>Items</td>
                 <td>
-                  {this.state.user_items}
+                  {JSON.stringify(this.state.user_items)}
                   {/* {this.state.user_items.map((user_item) => (
-                    <getItem user_item={user_item} />
+                    <UserItem user_item={user_item} />
                   ))} */}
                 </td>
               </tr>
