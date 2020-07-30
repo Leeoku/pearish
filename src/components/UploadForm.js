caa1eafe9e9510904ebfc53366d611b0a537e947
Kenneth Lee <leeoku@gmail.com>
2020-07-29 21:32:45 -0400

    add the react table CSS in case it doesnt work, polished the front end

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -23,6 +23,7 @@ const UploadForm = () => {
       // })
       .then((files) => {
         setFileInfos(files.data);
+        setMessage("Filed Uploaded!");
       })
       .catch(() => {
         setProgress(0);
